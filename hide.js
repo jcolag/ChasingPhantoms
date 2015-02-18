ffffbb26057432f7f74e5a8baa236c3fc608a76e
John Colagioia <john@colagioia.net>
2015-02-18 06:32:43 -0500

    First pass

---
+++ hide.js
@@ -0,0 +1,43 @@
+var radius = 275;
+var angle = - Math.PI / 2;
+var tick = 0.01;
+var offsetLeft = 300;
+var offsetTop = 10;
+var posX = 0;
+var posY = 0;
+
+var countHit = 0;
+var countMiss = 0;
+
+var reportScore = function() {
+  var score = "" + countHit + " hits<br>" + countMiss + " misses";
+  $("#target").html(score);
+}
+var boxCaught = function() {
+  countHit++;
+  countMiss--;
+  reportScore();
+}
+var boxMissed = function() {
+  countMiss++;
+  reportScore();
+}
+
+var setPosition = function() {
+  $("#target").offset({left: posX, top: posY});
+};
+var updateAngle = function(delta) {
+  angle += delta;
+  if (angle >= 2 * Math.PI) {
+    angle = 0.0;
+  }
+  var cos = Math.cos(angle);
+  var sin = Math.sin(angle);
+  posX = radius * cos + radius + offsetLeft;
+  posY = radius * sin + radius + offsetTop;
+};
+var move = function() {
+  updateAngle(tick);
+  setPosition();
+}
+
